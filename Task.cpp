e79fa93cc5833adbfb49b130c1c11ce43c6bd614
fclerget <114555886+fclerget@users.noreply.github.com>
2024-10-23 14:35:06 -0700

    completed Task.h or Task.cpp

---
+++ Task.cpp
@@ -0,0 +1,18 @@
+#include "Task.h"
+#include <iostream>
+
+Task::inline Task(int req) { requested = req; }
+
+int Task:: getRequested() {
+	return requested;
+}
+
+int Task:: getServiced() {
+	return serviced;
+}
+
+PriorityTask::PriorityTask(int req, int freq) : Task(req), frequency(freq) {}
+
+int PriorityTask:: getFrequency() {
+	return frequency;
+}
--- Task.h
+++ Task.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+
+class Task {
+private:
+	int requested;
+	int serviced;
+public:
+	Task(int req);
+	int getRequested();
+	int getServiced();
+};
+
+class PriorityTask : protected Task{
+private: 
+	//priority
+	int frequency;
+public:
+	PriorityTask(int req, int freq);
+	int getFrequency();
+}
+
