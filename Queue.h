5f0296ab2d3bdecb6fd9ad51193867cb8ec8b21f
fclerget <114555886+fclerget@users.noreply.github.com>
2024-10-31 10:15:13 -0700

    Added Queue.h

--- Queue.h
+++ Queue.h
@@ -1,1 +1,40 @@
+/**
+ * @file Queue.h
+ * @brief Contains the queue class definitions
+ *
+ * Contains the definition of the squeue class and the node struct.
+ * The queue class is used to store tasks in a FIFO order.
+ *
+ * @date 10/29/24
+ * @author Fiya Clerget
+ */
+
+
 #pragma once
+#include "Task.h"
+
+struct Node {
+    Task taskData;
+    Node* nextTask;
+};
+
+
+class Queue {
+public:
+
+    // Constructor and Destructor
+    Queue();
+    ~Queue();
+
+    // Push and Pop methods
+    void push(Task task);
+    void pop();
+
+    bool isEmpty();  // Bool to check if the queue is empty
+
+    void printQueue();  // Function to print the queue
+    void runExample();  // Function to run the example
+private:
+    Node* head;  // Top of the queue
+    Node* tail;  // Bottom of the queue (new)
+};
